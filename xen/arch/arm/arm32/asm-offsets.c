95583eef1f7345b8370efe14210a80d597da2ff5
Bamvor Jian Zhang <bjzhang@suse.com>
2013-07-02 21:00:56 +0800

    xen: arm: introduce Cortex-A7 support
    Introduce Cortex-A7 with a scalable proc_info_list which including cpu id
    and cpu initialize function.
    In head.S, search cpu specific MIDR in procinfo and call such initialize
    function. Currently, support Cortex-A7 and Cortex-A15.

--- xen/arch/arm/arm32/asm-offsets.c
+++ xen/arch/arm/arm32/asm-offsets.c
@@ -11,6 +11,7 @@
 #include <xen/bitops.h>
 #include <public/xen.h>
 #include <asm/current.h>
+#include <asm/arm32/procinfo.h>
 
 #define DEFINE(_sym, _val) \
     __asm__ __volatile__ ( "\n->" #_sym " %0 " #_val : : "i" (_val) )
@@ -62,6 +63,10 @@ void __dummy__(void)
    DEFINE(CPUINFO_sizeof, sizeof(struct cpu_info));
 
    OFFSET(VCPU_arch_saved_context, struct vcpu, arch.saved_context);
+
+   BLANK();
+   DEFINE(PROCINFO_sizeof, sizeof(struct proc_info_list));
+   OFFSET(PROCINFO_cpu_init, struct proc_info_list, cpu_init);
 }
 
 /*
